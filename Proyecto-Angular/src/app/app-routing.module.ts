20881671802069703475c42f51301fad66c4b19d
Bosbimi <bosbimi@gmx.es>
2022-10-22 18:48:11 -0600

    commit

--- Proyecto-Angular/src/app/app-routing.module.ts
+++ Proyecto-Angular/src/app/app-routing.module.ts
@@ -4,9 +4,12 @@ import { AcercademiComponent } from './Acercademi/Acercademi.component';
 import { AdministradorComponent } from './administrador/administrador.component';
 //import { ErrorComponent } from './error/error.component';
 
+
+
+
 const routes: Routes = [ 
   //{path: '', component: AcercademiComponent},
-  {path: 'administrador', component: AdministradorComponent},
+  {path: 'admin', component: AdministradorComponent},
   //{path: '', redirectTo: '/', pathMatch: 'full'},
   //{path: '**', component:ErrorComponent}, falta crear, importar y declarar ruta del componente error
   
